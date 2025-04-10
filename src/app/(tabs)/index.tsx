5921c255b6b119b68f7f17746cade6b0aa9994c5
Giovanni-Hernandez10 <giovannihernandez10209@gmail.com>
2025-04-10 09:03:20 -0700

    Basic prototype of an album cover

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,13 +3,11 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@components/EditScreenInfo';
 import { Text, View } from '@components/Themed';
 
-export default function TabOneScreen() {
+import AlbumCoverItem from '@/components/AlbumCoverItem';
+
+export default function AlbumCover() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <AlbumCoverItem />
   );
 }
 
--- src/components/AlbumCoverItem.tsx
+++ src/components/AlbumCoverItem.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
-import image from '@assets/images/splash-icon.png';
+import image from '@assets/images/headshot.jpg';
 
 // Component for album covers which will include an image, title, and last updated data field
 function AlbumCoverItem() {
     return (
-        <View>
+        <View style={styles.container}>
             <Image
+            style={styles.image}
             source={image} // add OR option which will display "No images"
             />
-            <Text>Peninsula Bridge 2025</Text>
-            <Text>Last updated: 10/22/23</Text>
+            <Text style={styles.title}>Professional Headshots</Text>
+            <Text style={styles.update}>Last updated: 10/22/23</Text>
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        borderRadius: 20
+    },
+    title: {
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+    update: {
+        fontSize: 10
+    },
+    image: {
+        maxWidth: '50%',
+        maxHeight: '30%',
+        borderRadius: 20
+    }
+    
+});
+
+
 export default AlbumCoverItem;
+
+
